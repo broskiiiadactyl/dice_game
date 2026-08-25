722b95359aeeb4b0fbef81c30284752c215add15
broskiiiadactyl <zandersfong@gmail.com>
2026-08-25 13:09:23 -0400

    still working on dice
    -they can be accessed by unique name reference now
    -syntax is %dN where N is the dice number

--- Scripts/spawner.gd
+++ Scripts/spawner.gd
@@ -1,43 +1,27 @@
 extends Node3D
 
-#This is a general spawner
-#Spawns objects on load
-#Add the scene of whatever you want to spawn in the inspector
+#This is a dice spawner
+#Spawns dice on command
 
 @export_group("Spawn Parameters")
-@export var spawn_scene: PackedScene
-@export var spawn_count := 5
-#@export var use_random_offset := false
-#@export var spawn_range := Vector3(0,0,0)
+@export var spawn_scene: PackedScene = load("res://Assets/Dice/Scenes/player_die.tscn")
 @export var use_random_rotation := false
-@export var rotation_range := Vector3(0,0,0)
+@export var rotation_range := Vector3(360,360,360)
 @export var spawn_timer := 0.0
 
 @export_group("Dice Placement")
 @export var radius : float = 0.1
 
 func _ready() -> void:
-	spawn_objects()
+	spawn_objects(5)
 
-func spawn_objects() -> void:
+func spawn_objects(spawn_count: int) -> void:
 	if spawn_scene == null:
 		push_warning("No scene to spawn. Assign a scene.")
 		return
 	
-	for i in range(1, spawn_count):
-		print(i)
-		#var x := spawn_range.x
-		#var y := spawn_range.y
-		#var z := spawn_range.z
-		#
-		#var pos = global_transform.origin
-		#if use_random_offset:
-			#pos += Vector3(
-				#randf_range(-x, x),
-				#randf_range(-y, y),
-				#randf_range(-z, z)
-			#)
-		var pos = place_dice(global_position, i - 1)
+	for i in range(spawn_count):
+		var pos = place_dice(global_position, i - 1, spawn_count)
 		
 		var rot_x := rotation_range.x
 		var rot_y := rotation_range.y
@@ -55,14 +39,16 @@ func spawn_objects() -> void:
 			await wait(spawn_timer)
 			
 		var instance := spawn_scene.instantiate()
-		#print(instance.name, " spawned at ", pos)
 		add_child(instance)
 		instance.global_position = pos
 		instance.global_rotation = rot
+		instance.name = str("d", i + 1)
+		instance.owner = self
+		instance.unique_name_in_owner = true
 
-func place_dice(center: Vector3, dice: int) -> Vector3:
+func place_dice(center: Vector3, dice: int, spawn_count: int) -> Vector3:
 	
-	var angle : float = ((2 * PI) / (spawn_count - 1)) * dice
+	var angle : float = ((2 * PI) / (spawn_count)) * dice
 	var point = Vector2(center.x, center.z) + Vector2(cos(angle), sin(angle)) * radius
 	return Vector3(point.x, center.y, point.y)
 
