9a88b7ab92464c3fae4d29c15067d1afcef9ff95
orkonomics <hoag.jeff@gmail.com>
2026-08-24 23:04:00 -0700

    set dice added

--- public_information.gd
+++ public_information.gd
@@ -1,1 +1,20 @@
-extends Resource
+extends Node
+
+var results_dict = {
+	"player" : [],
+	"npc1" : [],
+	"npc2" : [],
+	"npc3" : []
+}
+
+func _ready():
+	pass
+
+func set_dice( dice_value, target: = "player"):
+	if !dice_value:
+		push_error("Error: set_dice was called in player_die but no value was passed through.")
+	
+	results_dict[target].append(dice_value)
+	
+	# dict[key].clear() to remove values from an array
+	print( str(dice_value)+" adds to "+target+"'s result dictionary")
