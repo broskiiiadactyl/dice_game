dc1aa4947d1f5697ed8f37074ca8a341c9103a10
broskiiiadactyl <zandersfong@gmail.com>
2026-08-27 22:35:38 -0400

    Scene Management
    -Set Main Menu as first scene
    -built scene transition

--- Menuing/trans.gd
+++ Menuing/trans.gd
@@ -1,20 +1,39 @@
-extends Control
+extends CanvasLayer
 
 @onready var cover : ColorRect = %Cover
 
 @onready var audio : AudioStreamPlayer3D = %AudioStreamPlayer3D
 
-@export_file("*.tscn") var target_scene_path : String = "res://Main.tscn"
+var target_scene_path
 
 @onready var progress_bar : ProgressBar = %ProgressBar
 var progress : Array
+var loading : bool = false
 
-func _ready() -> void:
+func _process(_delta: float) -> void:
+	if loading:
+		var status = ResourceLoader.load_threaded_get_status(target_scene_path, progress)
+		
+		match status:
+			ResourceLoader.THREAD_LOAD_IN_PROGRESS:
+				progress_bar.value = progress[0] * 100
+			ResourceLoader.THREAD_LOAD_LOADED:
+				var loaded_resource = ResourceLoader.load_threaded_get(target_scene_path)
+				get_tree().change_scene_to_packed(loaded_resource)
+				self.visible = false
+				loading = false
+			ResourceLoader.THREAD_LOAD_FAILED, ResourceLoader.THREAD_LOAD_INVALID_RESOURCE:
+				push_error("Scene ", target_scene_path," failed to load! Status: ", status)
+				set_process(false)
+			_:
+				push_error("This shouldn't happen! Something went wrong with ResourceLoader! Status: ", status)
+
+func load_scene(target : String = "res://Main.tscn") -> void:
+	self.visible = true
+	await get_tree().process_frame
+	await get_tree().create_timer(2.0).timeout
+	loading = true
+	target_scene_path = target
 	var error = ResourceLoader.load_threaded_request(target_scene_path)
 	if error != OK:
-		push_error("Error loading scene ", target_scene_path, ": ", error)
-
-func _process(delta: float) -> void:
-	var status = ResourceLoader.load_threaded_get_status(target_scene_path, progress)
-	
-	
+		push_error("Error loading scene ", target, ": ", error)
