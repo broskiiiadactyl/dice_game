f32b4d4885f4a8ee4b016c477c372ee2473d4f64
broskiiiadactyl <zandersfong@gmail.com>
2026-08-28 22:14:11 -0400

    Jiggle Bone Addon

---
+++ Scripts/npc_1.gd
@@ -0,0 +1,32 @@
+extends Node3D
+
+var expressions : Dictionary = {
+	"Angry" : load("res://Assets/Characters/npc1/faces/Slade_Angy_Expr.png"),
+	"Angry Talk" : load("res://Assets/Characters/npc1/faces/Slade_AngyTalk_Expr.png"),
+	"Talk" : load("res://Assets/Characters/npc1/faces/Slade_Talk_Expr.png"),
+	"Talk 2" : load("res://Assets/Characters/npc1/faces/Slade_Talk_Expr2.png"),
+	"Neutral" : load("res://Assets/Characters/npc1/faces/Slade_Neutral_Expr.png")
+}
+
+@onready var player : AnimationPlayer = %AnimationPlayer
+@onready var head_material : Material = %Hair.get_active_material(1)
+
+func _ready() -> void:
+	expression_test()
+
+func change_expression(expression: String = "Neutral") ->void:
+	print(head_material.emission_texture, " ", expressions[expression])
+	head_material.emission_texture = expressions[expression]
+
+func expression_test() -> void:
+	while true:
+		for face in expressions:
+			print(face)
+			change_expression(face)
+			await get_tree().create_timer(2.0).timeout
+
+func play_animation(anim: String = "Idle") -> void:
+	if player.has_animation(anim):
+		player.play(anim)
+	else:
+		push_error("Invalid animation set to ", name, ": ", anim)
